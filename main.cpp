1e40dcee3389a53ad4d88a39b5eba78250f2a622
LIU2212138 <105722618+LIU2212138@users.noreply.github.com>
2022-12-18 02:59:11 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(){
     sadMat<int> intMatrix(3,2, true);
     sadMat<float> floatMatrix(4,3, true);
     //左的列数要等于右的行数
+    //*****重载双目算术运算符***************************************************************
     sadMat<float> product = floatMatrix*intMatrix;
 
     cout<<"--------intMatrix--------"<<endl
@@ -18,7 +19,9 @@ int main(){
         <<floatMatrix<<endl;
     cout<<"---------product---------"<<endl
         <<product<<endl;
-
+    
+    
+    //*****重载关系运算符以及等于号（=）******************************************************************
     sadMat<int> intMatrix2(3,2, false) ;
     intMatrix2 = intMatrix;
     bool isEqual = intMatrix2==intMatrix;
@@ -29,17 +32,18 @@ int main(){
     cout<<"The Equality of intMatrix and intMatrix2: "<<isEqual<<endl;
     cout<<"The Inequality of floatMatrix and intMatrix2: "<<isNotEqual<<endl;
 
+    //*****ROI截取****************************************************************************************
     sadMat<float> roi (floatMatrix,0,1,2,2);
     cout<<"--------ROI of floatMatrix--------"<<endl
-        <<roi<<endl;
-
+        <<roi<<endl;*
+    //*****set get 函数***********************************************************************************
     cout<<"-------the (1,2) element of intMatrix------"<<endl
         <<intMatrix.get(1,2)<<endl;
 
     intMatrix.set(1,2,0);
     cout<<"-------After set to 0, the (1,2) element of intMatrix------"<<endl
         <<intMatrix.get(1,2)<<endl;
-
+    //*****重载赋值运算符**********************************************************************************
     roi+=roi;
     cout<<"-------After adding itself, the roi Matrix------"<<endl
         <<roi<<endl;
