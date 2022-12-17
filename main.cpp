ade7d0c1295cd4b2def76068f2344628e0cb75a0
LIU2212138 <105722618+LIU2212138@users.noreply.github.com>
2022-12-18 03:47:12 +0800

    Update main.cpp
    改部分测试

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Created by 86187 on 2022/12/17.
 //
 #include "iostream"
-#include "sadMat.h"
+#include "sadMat.cpp"
 
 using namespace std;
 int main(){
@@ -19,8 +19,8 @@ int main(){
         <<floatMatrix<<endl;
     cout<<"---------product---------"<<endl
         <<product<<endl;
-    
-    
+
+
     //*****重载关系运算符以及等于号（=）******************************************************************
     sadMat<int> intMatrix2(3,2, false) ;
     intMatrix2 = intMatrix;
@@ -37,14 +37,17 @@ int main(){
     cout<<"--------ROI of floatMatrix--------"<<endl
         <<roi<<endl;
     //*****set get 函数***********************************************************************************
-    cout<<"-------the (1,2) element of intMatrix------"<<endl
-        <<intMatrix.get(1,2)<<endl;
+    cout<<"-------the (1,1) element of intMatrix------"<<endl
+                <<intMatrix.get(1,1)<<endl;
 
-    intMatrix.set(1,2,0);
-    cout<<"-------After set to 0, the (1,2) element of intMatrix------"<<endl
-        <<intMatrix.get(1,2)<<endl;
+    intMatrix.set(1,1,0);
+    cout<<"-------After set to 0, the intMatrix------"<<endl
+        <<intMatrix<<endl;
+
     //*****重载赋值运算符**********************************************************************************
     roi+=roi;
     cout<<"-------After adding itself, the roi Matrix------"<<endl
         <<roi<<endl;
+    //*****越界**********************************************************************************
+    intMatrix.get(1,2);
 }
